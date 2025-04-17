c9161bf0ba1a203563c66d2047f53d4c5c88af4b
y365y <y365y@126.com>
2025-04-17 10:29:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from playwright.sync_api import sync_playwright
 
 def handle_page(page):
     # 从环境变量中获取Cookie信息
-    cookie_value = os.getenv('ck')
-    url = os.getenv('url')
+    cookie_value = os.getenv('CK')
+    url = os.getenv('URL')
+    print(f"{url} {cookie_value}")
     if not cookie_value:
         raise ValueError("环境变量 'ck' 未设置，请确保已正确配置。")
 
